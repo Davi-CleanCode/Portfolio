beecdf69a3ed9f58ad37a478207445c09fd5d8cd
davi viana <davivina2732@gmail.com>
2025-05-03 17:15:17 -0300

    atualizando pdf cv

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll';
 import { Download, ArrowDown, Code, Mail, Briefcase } from 'lucide-react';
 import { motion } from 'framer-motion';
 import fotoPerfil from '../assets/fotoperfil.jpeg';
-import curriculo from '../assets/CV-DaviViana.pdf'
+import curriculo from '../assets/Davi_Cv.pdf'
 
 const HeroSection: React.FC = () => {
   const handleResumeDownload = () => {
